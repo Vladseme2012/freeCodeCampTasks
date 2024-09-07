e45b3e9acb22ac885f5ec4377635c7a01d8889e4
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-07 19:00:26 +0300

    added task1 from freeCodeCamp

---
+++ task1.js
@@ -0,0 +1,8 @@
+// 1.Print the value of the character variable to the console. Then, click the "Console" button to view the JavaScript console.
+let character = 'Hello';
+console.log(character)
+
+// running tests
+// tests completed
+// console output
+Hello
