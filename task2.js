8bf42e2dd23428ba3e0ec7241497c53b8feca79e
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-07 21:09:44 +0300

    added task2 from freeCodeCamp

---
+++ task2.js
@@ -0,0 +1,10 @@
+// 1. Note that when reassigning a variable, you do not use the let keyword again.
+
+// After your console.log, assign the value "World" to your character variable.
+let character = 'Hello';
+console.log(character);
+character = "World";
+// running tests
+// tests completed
+// console output
+Hello
