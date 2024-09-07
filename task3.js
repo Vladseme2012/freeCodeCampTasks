d2b955217122688e907beece143547313d027809
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-07 21:18:05 +0300

    added task3 from freeCodeCamp

---
+++ task3.js
@@ -0,0 +1,12 @@
+// 1.Step 7
+// Now log your character variable to the console again. You should see the string "Hello", then the string "World", in the console.
+let character = 'Hello';
+console.log(character);
+character = "World";
+console.log(character);
+// running tests
+// tests completed
+// console output
+Hello
+World
+// Learn Introductory JavaScript by Building a Pyramid Generator - 6% complete.
