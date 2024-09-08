8337827577cfabe956d10d25eb391d432916e53f
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-08 15:20:27 +0300

    added task5 with the solution from freeCodeCamp

---
+++ task5.js
@@ -0,0 +1,10 @@
+// 1. Step 9.
+// Add a console.log() to see what the value of your secondCharacter variable is.
+
+// The solution:
+Hello;
+undefined;
+
+// running tests
+// tests completed - 8% complete
+// console output
