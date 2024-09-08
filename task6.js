6faf3305c338913f0e251128440fa25b8677f36c
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-08 15:31:58 +0300

    added task6 with the solution from freeCodeCamp

---
+++ task6.js
@@ -0,0 +1,9 @@
+// 1.Step 10
+// Assign the string "Test" to your secondCharacter variable below your declaration. Open the console to see how your log has changed.
+
+// The solution:
+let secondCharacter;
+secondCharacter = "Test";
+
+// running tests
+// tests completed - 8% complete
