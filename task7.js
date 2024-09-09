02f3105bf7e5e9f400af52e8887f232efb414d42
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-09 18:57:07 +0300

    added the solution for step 10 from freeCodeCamp

---
+++ task7.js
@@ -0,0 +1,15 @@
+// 1.Step 10
+// You can also assign the value of a variable to another variable. For example:
+// The second variable would now have the value "One".
+
+// To see this in action, change your secondCharacter assignment from "Test" to your character variable.
+
+// Then open the console to see what gets logged.
+
+// The solution:
+secondCharacter = character;
+
+// running tests
+// tests completed - 9% complete;
+// console output
+
