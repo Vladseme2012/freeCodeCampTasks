84d2710797d09054eb822ba08334f590dbfa8192
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-10 18:43:44 +0300

    added solution for step 15 from freeCodeCamp

---
+++ task11.js
@@ -0,0 +1,12 @@
+// 1.Step 15
+// Use let to declare a count variable. Assign it the number 8. When using a number value, you do not use quotes. For example:
+
+// Example Code
+// let money = 100;
+
+
+
+// The solution:
+let money = 100;
+let count = 8;
+// 13% complete
