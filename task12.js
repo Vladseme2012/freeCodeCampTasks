bbc1e295bd00ae4327f1dac6f97f0ad6f63de5a2
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-10 18:47:24 +0300

    added solution for step 16 from freeCodeCamp

---
+++ task12.js
@@ -0,0 +1,12 @@
+// 1.Step 16
+// With the number data type, you can perform mathematical operations, like addition. Try printing count + 1 to the console.
+
+// The solution:
+let character = 'Hello';
+let count = 8;
+console.log(count + 1);
+// running tests
+// tests completed
+// console output
+// 9
+// 14% complete
