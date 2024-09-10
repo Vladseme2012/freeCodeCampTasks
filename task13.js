d170aad6360bf5e28133971012f89dcaf98c6495
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-10 18:51:37 +0300

    added solution for step 17 from freeCodeCamp

---
+++ task13.js
@@ -0,0 +1,9 @@
+// 1.Step 17
+// You can also perform subtraction (-), multiplication (*), and division (/). Feel free to experiment with the operators and numbers in your console.log. When you are ready to move on, remove the console.log.
+
+// The solution:
+let count = 8;
+console.log(count + 1); //удален console.log(count + 1);
+// running tests
+// tests completed
+// 14% complete
