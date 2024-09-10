6e7367746f24c59c46ebd7bb71f9d43dc066f048
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-10 19:01:05 +0300

    added solution for step 19 from freeCodeCamp

---
+++ task15.js
@@ -0,0 +1,12 @@
+// 1.Step 19
+// When an array holds values, or elements, those values are separated by commas. Here is an array that holds two strings:
+
+// Example Code
+// let array = ["first", "second"];
+// Change your rows declaration to be an array with the strings "Naomi", "Quincy", and "CamperChan". The order of values in an array is important, so follow that order. Remember that strings are case-sensitive.
+
+// The solution:
+let rows = ["Naomi", "Quincy", "CamperChan"];
+// running tests
+// tests completed
+// 16% complete
