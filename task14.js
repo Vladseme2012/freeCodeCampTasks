ce440305c0bd18630d906335698b765c40b54457
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-10 18:54:52 +0300

    added solution for step 18 from freeCodeCamp

---
+++ task14.js
@@ -0,0 +1,14 @@
+// 1.Step 18
+// An array is a non-primitive data type that can hold a series of values. Non-primitive data types differ from primitive data types in that they can hold more complex data. Primitive data types like strings and numbers can only hold one value at a time.
+
+// Arrays are denoted using square brackets ([]). Here is an example of a variable with the value of an empty array:
+
+// Example Code
+// let array = [];
+// Declare a rows variable and assign it an empty array.
+
+// The solution:
+let rows = [];
+// running tests
+// tests completed
+// 15% complete
