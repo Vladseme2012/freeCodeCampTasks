a55af7a3482e55089b8579031a31ede83040a6c1
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-11 19:37:04 +0300

    added the solution on the step 21 from freeCodeCamp

---
+++ task17.js
@@ -0,0 +1,19 @@
+// 1.Step 21
+// Arrays are special in that they are considered mutable. This means you can change the value at an index directly.
+
+// For example, this code would assign the number 25 to the second element in the array:
+
+// Example Code
+// let array = [1, 2, 3];
+// array[1] = 25;
+// console.log(array); // prints [1, 25, 3]
+// Update the third element of your rows array to be the number 10. Then print the rows array to your console.
+
+// The solution:
+rows[2] = 10;
+console.log(rows);
+// running tests
+// tests completed
+// console output
+Naomi[('Naomi', 'Quincy', 10)];
+// 18% complete
