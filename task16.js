19e825d517d497c0dcf948d34d92d3e4838b58a1
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-11 19:16:14 +0300

    added the solution on the step 20 from freeCodeCamp

---
+++ task16.js
@@ -0,0 +1,15 @@
+// 1.Step 20
+// You can access the values inside an array using the index of the value. An index is a number representing the position of the value in the array, starting from 0 for the first value.
+
+// You can access the value using bracket notation, such as array[0].
+
+// Use console.log and bracket notation to print the first value in your rows array.
+
+// The solution:
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows[0]);
+// running tests
+// tests completed
+// console output
+Naomi
+// 17% complete
