936104a7ea7f36491ffc9b6922ae67942fbddec4
Vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-12 16:30:58 +0300

    added the solution for the task20 from freeCodeCamp

---
+++ task20.js
@@ -0,0 +1,25 @@
+// 1.Step 24
+// In the last few steps, you learned all about working with arrays. Take a moment to review what you have learned.
+
+// Start by declaring a cities variable and initializing it as an array of the strings "London", "New York", and "Mumbai". Then log that variable to the console.
+
+// After logging, change the last element of cities to the string "Mexico City", then log the cities variable again.
+
+// When done correctly, you should see this output in the console.
+
+// Example Code
+// [ "London", "New York", "Mumbai" ]
+// [ "London", "New York", "Mexico City" ]
+
+// The solution:
+let cities = ['London', 'New York', 'Mumbai'];
+console.log(cities);
+cities[cities.length - 1] = 'Mexico City';
+console.log(cities);
+// running tests
+// tests completed
+// console output
+['London', 'New York', 'Mumbai'][('London', 'New York', 'Mexico City')][
+    ('Naomi', 'Quincy', 'CamperChan')
+];
+// 20% complete
