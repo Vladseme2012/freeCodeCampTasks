88085014974a1f47514cad54e4f2d3b0ac778598
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-13 20:58:46 +0300

    added the solution for the task 19 from freeCodeCamp

---
+++ task19.js
@@ -0,0 +1,10 @@
+// 1.Step 23
+// For now, remove your first console log and your rows[rows.length - 1] assignment. Leave the second rows log statement for later.
+
+// The solution:
+
+// running tests
+// tests completed
+// console output
+[ 'Naomi', 'Quincy', 'CamperChan' ]
+// 19% complete
