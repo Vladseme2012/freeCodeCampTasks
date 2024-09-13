2ff6508de0e3db22547a6a22d3e62359d9cace9a
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-13 21:09:19 +0300

    added the solution for the task 23 from freeCodeCamp

---
+++ task23.js
@@ -0,0 +1,19 @@
+// 1.Step 27
+// Another method essential for this project is the .pop() method. It removes the last element from an array and returns that element.
+
+// When a method returns a value, you can think of it as giving the value back to you, making it available for use in other parts of your code.
+
+// Create a new variable called popped and assign it the result of rows.pop(). Then, log popped to the console.
+
+// The solution:
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+console.log(rows);
+let popped = rows.pop();
+console.log(popped);
+// running tests
+// tests completed
+// console output
+[ 'Naomi', 'Quincy', 'CamperChan', 'freeCodeCamp' ]
+freeCodeCamp
+// 22% complete
