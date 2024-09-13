ba9750149fe103e319dfa96b0d89ba614392b878
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-13 20:59:52 +0300

    added the solution for the task 21 from freeCodeCamp

---
+++ task21.js
@@ -0,0 +1,10 @@
+// 1.Step 25
+// Now you are ready to move onto the next set of array lessons.
+
+// Remove all of your code from the previous step.
+
+// The solution:
+
+// running tests
+// tests completed
+// 21% complete
