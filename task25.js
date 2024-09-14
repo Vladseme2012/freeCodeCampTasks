e01d1a9befd2f5c61063ffbbd93147cf34415cfe
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-14 13:52:10 +0300

    added the solution for the step 29 from freeCodeCamp

---
+++ task25.js
@@ -0,0 +1,10 @@
+// 1.Step 29
+// Were you expecting to see 4 in the console? .push() returns the new length of the array, after adding the value you give it.
+
+// It is important to be aware of what values a method returns. Take some time to experiment with .push() and .pop(). When you are ready, remove all of your .push() and .pop() calls, and your console.log statements.
+
+// The solution:
+
+// running tests
+// tests completed
+// 25% complete
