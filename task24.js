fea4fffde00b673ff8fbe72a4297abb7b8a2c910
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-14 13:47:45 +0300

    added the solution for the step 28 from freeCodeCamp

---
+++ task24.js
@@ -0,0 +1,15 @@
+// 1.Step 28
+// You should have seen "freeCodeCamp" printed to the console. This is because .pop() returns the value that was removed from the array - and you pushed "freeCodeCamp" to the end of the array earlier.
+
+// But what does .push() return? Assign your existing rows.push() to a new pushed variable, and log it.
+
+// The solution:
+rows.push('freeCodeCamp');
+let pushed = rows.push();
+console.log(pushed);
+// running tests
+// tests completed
+// console output
+4;
+freeCodeCamp[('Naomi', 'Quincy', 'CamperChan')];
+// 24% complete
