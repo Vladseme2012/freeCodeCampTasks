2260abab1ccd72b534516af90c5d9588618babb4
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-14 16:13:08 +0300

    added the solution for the step 32 from freeCodeCamp

---
+++ task28.js
@@ -0,0 +1,12 @@
+// 1.Step 32
+// You are now ready to start building your pyramid generator. Your character variable will serve as the building block for the pyramid.
+
+// "Hello" might not work very well for that. Change the value of character to be the hash character "#".
+
+// The solution:
+const character = '#';
+const count = 8;
+const rows = [];
+// running tests
+// tests completed
+// 27% complete
