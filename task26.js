011705130d42e3e55dc60b0e6eb4ec5826a1f9dd
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-14 13:56:10 +0300

    added the solution for the step 30 from freeCodeCamp

---
+++ task26.js
@@ -0,0 +1,12 @@
+// 1.Step 30
+// Change your rows declaration to be assigned an empty array again.
+
+// Also, change your 'Hello' string to use double quotes again. Generally, it does not matter which of the two you prefer, but you will want to be consistent in that choice throughout your project.
+
+// The solution:
+let character = 'Hello';
+let count = 8;
+let rows = [];
+// running tests
+// tests completed
+// 25% complete
