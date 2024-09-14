fd877d3b4e7c0494e79c29086e34bc5b2d5ec77a
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-14 16:40:43 +0300

    added the solution for the step 35 from freeCodeCamp

---
+++ task30.js
@@ -0,0 +1,16 @@
+// 1.Step 35
+// The condition of a for loop tells the loop how many times it should iterate. When the condition becomes false, the loop will stop.
+
+// In JavaScript, a Boolean value can be either true or false. These are not strings - you will learn more about the difference later on.
+
+// For now, you will use the less than operator (<). This allows you to check if the value on the left is less than the value on the right. For example, count < 3 would evaluate to true if count is 2, and false if count is 4.
+
+// Replace your "condition" string with a condition to check if i is less than count.
+
+// The solution:
+for (let i = 0; i < count; 'iteration') {}
+// running tests
+// tests completed
+// console output
+// Potential infinite loop detected on line 5. Tests may be failing because of this.
+// 29% complete
