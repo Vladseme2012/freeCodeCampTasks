dfeb058bc9ec7f4d596e3d85b83b79f4ab893dfb
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-15 11:37:07 +0300

    added the solution for the step 38 from freeCodeCamp

---
+++ task33.js
@@ -0,0 +1,12 @@
+// 1.Step 38
+// You should see the numbers zero through seven printed in your console, one per line. This will serve as the foundation for generating your pyramid.
+
+// Replace your log statement with a statement to push i to your rows array.
+
+// The solution:
+for (let i = 0; i < count; i = i + 1) {
+    rows.push(i);
+}
+// running tests
+// tests completed
+// 32% complete
