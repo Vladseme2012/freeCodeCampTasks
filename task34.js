9977aa37634490c00271b431c1b71825f70709aa
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-15 11:45:59 +0300

    added the solution for the step 39 from freeCodeCamp

---
+++ task34.js
@@ -0,0 +1,10 @@
+// 1.Step 39
+// Unfortunately, now you cannot see what your loop is doing.
+
+// Use let to declare a result variable, and assign it an empty string. An empty string is represented by quotation marks with nothing between them, such as "".
+
+// The solution:
+let result = '';
+// running tests
+// tests completed
+// 33% complete
