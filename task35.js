1a537afd7aa732bc6b9fdd0f1cdb779a5ae7104f
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-15 11:48:26 +0300

    added the solution for the step 40 from freeCodeCamp

---
+++ task35.js
@@ -0,0 +1,9 @@
+// 1.Step 40
+// Add a log statement to print the value of result. Depending on which console you use, you may not see anything printed.
+
+// The solution:
+console.log(result);
+// running tests
+// tests completed
+// console output
+// 34% complete
