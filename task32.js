5ad5482f974a1508c03ae2c6c764402a8cc4359e
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-15 11:34:06 +0300

    added the solution for the step 37 from freeCodeCamp

---
+++ task32.js
@@ -0,0 +1,19 @@
+// 1.Step 37
+// Your loop should now run eight times. Inside the body of the loop, print the value of the i iterator and see what happens.
+
+// The solution:
+for (let i = 0; i < count; i = i + 1) {
+    console.log(i);
+}
+// running tests
+// tests completed
+// console output
+0;
+1;
+2;
+3;
+4;
+5;
+6;
+7;
+// 31% complete
