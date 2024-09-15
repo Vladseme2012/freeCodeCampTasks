0e656618d0a69caa76f6c4a8c0148b6c21c57fba
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-15 12:41:56 +0300

    added the solution for the step 50 from freeCodeCamp

---
+++ task45.js
@@ -0,0 +1,13 @@
+// 1.Step 50
+// Now add a log statement to print the value of your call variable.
+
+// The solution:
+function padRow() {}
+const call = padRow();
+console.log(call);
+
+// running tests
+// tests completed
+// console output
+undefined;
+// 42% complete
