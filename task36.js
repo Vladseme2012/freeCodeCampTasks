30c3839fc55663a995382d54c25ec8dd5071612c
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-15 11:53:14 +0300

    added the solution for the step 41 from freeCodeCamp

---
+++ task36.js
@@ -0,0 +1,20 @@
+// 1.Step 41
+// To manipulate the result string, you will use a different type of loop. Specifically, a for...of loop, which iterates over each item in an iterable object and temporarily assigns it to a variable.
+
+// The syntax for a for...of loop looks like:
+
+// Example Code
+// for (const value of iterable) {
+
+// }
+// Note that you can use const because the variable only exists for a single iteration, not during the entire loop.
+
+// Create a for...of loop to iterate through your rows array, assigning each value to a row variable.
+
+// The solution:
+for (const row of rows) {
+}
+// running tests
+// tests completed
+// console output
+// 35% complete
