fba31794da5b1a788d303adfb411285d1fbfd1c9
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-18 19:32:04 +0300

    added the solution for the step 53 from freeCodeCamp

---
+++ task48.js
@@ -0,0 +1,12 @@
+// 1.Step 53
+// A function does not have to return a hard-coded value. It can return the value stored in a variable. Parameters are special variables for a function, so they can also be returned.
+
+// Change your padRow function to return the name parameter directly.
+
+// The solution:
+function padRow(name) {
+    return name;
+}
+// running tests
+// tests completed
+// 45% complete
