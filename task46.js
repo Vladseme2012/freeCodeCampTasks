7d42bdf37464f7f53c3f7a7d9c6227c8e89947e8
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-18 19:24:05 +0300

    added the solution for the step 51 from freeCodeCamp

---
+++ task46.js
@@ -0,0 +1,28 @@
+// 1.Step 51
+// Your call variable has an undefined value, even though you defined it! This is because your padRow function does not currently return a value. By default, functions return undefined as their value.
+
+// In order to return something else, you need to use the return keyword. Here is an example of a function that returns the string "Functions are cool!":
+
+// Example Code
+// function demo() {
+//   return "Functions are cool!";
+// }
+// Use the return keyword to have your function return the string "Hello!".
+
+// The solution:
+function padRow() {
+    return 'Hello!';
+}
+// running tests
+// tests completed
+// console output
+// Hello!
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+// ########
+// 43% complete
