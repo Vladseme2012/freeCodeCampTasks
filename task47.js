36d9ab01d4864f6c2c273c1f82947b51d5c53d19
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-18 19:30:18 +0300

    added the solution for the step 52 from freeCodeCamp

---
+++ task47.js
@@ -0,0 +1,20 @@
+// 1.Step 52
+// When you have a value that is explicitly written in your code, like the "Hello!" string in your function, it is considered to be hard-coded. Hard-coding a value inside a function might not make it as reusable as you'd like.
+
+// Instead, you can define parameters for the function. Parameters are special variables that are given a value when you call the function, and can be used in your function to dynamically change the result of the function's code.
+
+// To add a parameter to your function, you need to add a variable name inside the parentheses. For example, this demo function has a name parameter:
+
+// Example Code
+// function demo(name) {
+
+// }
+// name sounds like a useful parameter, so go ahead and add it to your padRow function.
+
+// The solution:
+function padRow(name) {
+  return "Hello!";
+}
+// running tests
+// tests completed
+// 43% complete
