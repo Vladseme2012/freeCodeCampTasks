c9709d90ed0598b1f54674466a3ff7200498c28f
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-19 18:34:50 +0300

    added the solution for the step 56 from freeCodeCamp

---
+++ task51.js
@@ -0,0 +1,10 @@
+// 1.Step 56
+// With that quick review complete, you should remove your addTwoNumbers function, sum variable, and log statement.
+
+// The solution:
+
+// running tests
+// tests completed
+// console output
+undefined;
+// 47% complete
