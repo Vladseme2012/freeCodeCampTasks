78628f776f409af4e8944283afcc32e27932c271
Vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-20 11:50:00 +0300

    added the solution for the step 62 from freeCodeCamp

---
+++ task57.js
@@ -0,0 +1,14 @@
+// 1.Step 62
+// Because your function was no longer using the parameter, changing the argument did not affect it.
+
+// Go ahead and remove the test declaration and return statement from your padRow function, so the function is empty again.
+
+// The solution:
+function padRow() {}
+const call = padRow();
+console.log(call);
+// running tests
+// tests completed
+// console output
+undefined;
+// 53% complete
