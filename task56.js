1d8c0e2b009fd0397d3f0fd9a0ad5ef5a580952d
Vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-20 11:38:40 +0300

    added the solution for the step 61 from freeCodeCamp

---
+++ task56.js
@@ -0,0 +1,18 @@
+// 1.Step 61
+// Now your call variable has the value "Testing". But your function is no longer using the name parameter.
+
+// Remove the name parameter from your function declaration, then remove your "CamperChan" string from the padRow call.
+
+// Also, remove both console.log from the padRow function.
+
+// The solution:
+function padRow() {
+  const test = "Testing";
+  return test;
+  
+}
+padRow();
+// running tests
+// tests completed
+// console output
+// 52% complete
