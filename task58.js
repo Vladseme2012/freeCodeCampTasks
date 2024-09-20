079b7046ec362eb3a8a91c8ce2ce5f698d73399a
Vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-20 12:03:43 +0300

    added the solution for the step 63 from freeCodeCamp

---
+++ task58.js
@@ -0,0 +1,9 @@
+// 1.Step 63
+// As expected, your function now returns undefined again. Your call variable is not necessary any more, so remove the call declaration and the console.log for the call variable.
+
+// The solution:
+function padRow() {}
+// running tests
+// tests completed
+// console output
+// 53% complete
