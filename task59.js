c651499bac70ef9f58981f351ebabbc7de9253f9
Vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-21 08:59:27 +0300

    added the solution for the step 64 from freeCodeCamp

---
+++ task59.js
@@ -0,0 +1,16 @@
+// 1.Step 64
+// In order to know how to format a row, your padRow function will need to know which row number you are on, and how many rows in total are being generated.
+
+// The best way to do this is by creating function parameters for them. Give your padRow function a rowNumber and rowCount parameter. Multiple parameters are separated by a comma:
+
+// Example Code
+// function name(first, second) {
+
+// }
+
+// The solution:
+function padRow(rowNumber, rowCount) {}
+// running tests
+// tests completed
+// console output
+// 54% complete
