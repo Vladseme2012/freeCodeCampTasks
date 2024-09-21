c4ee19d56f90e106b8f6e0edda8d0eaa4a575f51
Vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-21 09:01:23 +0300

    added the solution for the step 65 from freeCodeCamp

---
+++ task60.js
@@ -0,0 +1,19 @@
+// 1.Step 65
+// Remember in an earlier step, you learned about return values. A function can return a value for your application to consume separately.
+
+// In a function, the return keyword is used to specify a return value. For example, this function would return the value given to the first parameter:
+
+// Example Code
+// function name(parameter) {
+//   return parameter;
+// }
+// Use the return keyword to return the value of the character variable, repeated rowNumber times.
+
+// The solution:
+function padRow(rowNumber, rowCount) {
+    return character.repeat(rowNumber);
+}
+// running tests
+// tests completed
+// console output
+// 54% complete
