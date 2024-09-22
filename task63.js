3dbca9db4d313491c09458fa1bbf3726842a541d
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-22 18:31:37 +0300

    task63.js

---
+++ task63.js
@@ -0,0 +1,17 @@
+// 1.Step 68
+// Your padRow function has two parameters which you defined. Values are provided to those parameters when a function is called.
+
+// The values you provide to a function call are referred to as arguments, and you pass arguments to a function call. Here's a function call with "Hello" passed as an argument:
+
+// Example Code
+// test("Hello");
+// Pass i + 1 and count as the arguments to your padRow call. Like parameters, arguments are separated by a comma.
+
+// The solution:
+function padRow(rowNumber, rowCount) {
+    return ' ' + character.repeat(rowNumber) + ' ';
+}
+// running tests
+// tests completed
+// console output
+// 58% complete
