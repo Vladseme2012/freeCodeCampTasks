bb55604844e6231759107c42794c4b42e9a92f29
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-24 18:41:01 +0300

    added the solution for the step 78 from freeCodeCamp

---
+++ task73.js
@@ -0,0 +1,19 @@
+// 1.Step 78
+// Your pyramid has disappeared again. That's okay - that is to be expected.
+
+// Before you create your new loop, you need to learn about if statements. An if statement allows you to run a block of code only when a condition is met. They use the following syntax:
+
+// Example Code
+// if (condition) {
+//   logic
+// }
+
+// The solution:
+if (true) {
+    console.log('Condition is true');
+}
+// running tests
+// tests completed
+// console output
+('Condition is true');
+// 64% complete
