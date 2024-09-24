9ccdcf6ddf12b0311ac10dcd4edbe8bb7d70ce13
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-24 18:35:57 +0300

    added the solution for the step 76 from freeCodeCamp

---
+++ task71.js
@@ -0,0 +1,15 @@
+// 1.Step 76
+// Comments can be helpful for explaining why your code takes a certain approach, or leaving to-do notes for your future self.
+
+// In JavaScript, you can use // to leave a single-line comment in your code.
+
+// Add a single-line comment above your function to remind yourself to change the code to a different kind of loop.
+
+// The solution:
+// for (let i = 1; i <= count; i++) {
+//   rows.push(padRow(i, count));
+// }
+// running tests
+// tests completed
+// console output
+// 64% complete
