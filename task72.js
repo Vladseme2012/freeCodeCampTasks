050b76c71cb68cc0f099e5f2469a3b432ed2ef49
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-24 18:36:54 +0300

    added the solution for the step 77 from freeCodeCamp

---
+++ task72.js
@@ -0,0 +1,15 @@
+// 1.Step 77
+// JavaScript also has support for multi-line comments. A multi-line comment starts with /* and ends with */.
+
+// Unlike a single-line comment, a multi-line comment will encapsulate multiple lines.
+
+// Use /* and */ to turn your current for loop, including the body, into a multi-line comment.
+
+// The solution:
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+// running tests
+// tests completed
+// console output
+// 65% complete
