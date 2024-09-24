01570fce8577044d9d57a12f85d009e43d16d0d6
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-24 18:41:51 +0300

    added the solution for the step 79 from freeCodeCamp

---
+++ task74.js
@@ -0,0 +1,12 @@
+// 1.Step 79
+// You'll see the string printed in the console, because true is in fact true.
+
+// Change the condition of your if statement to the boolean false.
+
+// The solution:
+if (false) {
+    console.log('Condition is true');
+}
+// running tests
+// tests completed
+// 67% complete
