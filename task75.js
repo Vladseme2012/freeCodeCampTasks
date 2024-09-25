4e51abbe4b1e8f4c4633d47438dc39e9bd2e3df4
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-25 18:58:49 +0300

    added the solution for the step 80 from freeCodeCamp

---
+++ task75.js
@@ -0,0 +1,12 @@
+// 1.Step 80
+// Now the string is no longer printing, because false is not true. But what about other values?
+
+// Try changing the condition to the string "false".
+
+// The solution:
+if ('false') {
+    console.log('Condition is true');
+}
+// running tests
+// tests completed
+// 68% complete
