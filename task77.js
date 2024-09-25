a2e4a10e35f0a290571839b73afd95fa7b132bb0
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-25 19:00:24 +0300

    added the solution for the step 82 from freeCodeCamp

---
+++ task77.js
@@ -0,0 +1,32 @@
+// 1.Step 82
+// The text is gone again! Empty strings evaluate to false, making them a falsy value. You will learn more about truthy and falsy values in future projects.
+
+// In addition to if statements, JavaScript also has else if statements. else if statements allow you to check multiple conditions in a single block of code.
+
+// Here is the syntax for an else if statement:
+
+// Example Code
+// if (condition1) {
+//   // code to run if condition1 is true
+// } else if (condition2) {
+//   // code to run if condition2 is true
+// } else if (condition3) {
+//   // code to run if condition3 is true
+// }
+// If the first condition is false, JavaScript will check the next condition in the chain. If the second condition is false, JavaScript will check the third condition, and so on.
+
+// Below your if statement, add an else if statement that checks if 5 is less than 10. Then inside the body of the else if statement, log the string "5 is less than 10" to the console.
+
+// Check the console to see the results.
+
+// The solution:
+if ('') {
+    console.log('Condition is true');
+} else if (5 < 10) {
+    console.log('5 is less than 10');
+}
+// running tests
+// tests completed
+// console output
+('5 is less than 10');
+// 69% complete
