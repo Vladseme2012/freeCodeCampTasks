24cbee3d14502fd321534a460fce9b100e36f9ba
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-26 18:34:23 +0300

    added the solution for the step 84 from freeCodeCamp

---
+++ task79.js
@@ -0,0 +1,13 @@
+// 1.Step 84
+// Now that you have practiced working with if...else if...else statements, you can remove them from your code.
+
+// Once you complete that, use let to declare a continueLoop variable and assign it the boolean false. Then use let to declare a done variable and assign it the value 0.
+
+// The solution:
+let continueLoop = false;
+let done = 0;
+// running tests
+// tests completed
+// console output
+
+// 71% complete
