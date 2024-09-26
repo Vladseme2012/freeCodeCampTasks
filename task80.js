7eb16f241fa3de20987f9b36b550998d9032202c
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-26 18:35:23 +0300

    added the solution for the step 85 from freeCodeCamp

---
+++ task80.js
@@ -0,0 +1,18 @@
+// 1.Step 85
+// A while loop will run over and over again until the condition specified is no longer true. It has the following syntax:
+
+// Example Code
+// while (condition) {
+//   logic;
+// }
+// Use that syntax to declare a while loop with continueLoop as the condition. The body should be empty.
+
+// The solution:
+let continueLoop = false;
+let done = 0;
+while(continueLoop) {}
+// running tests
+// tests completed
+// console output
+
+// 72% complete
