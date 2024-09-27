1c8e26d66fe899a70d0454447eccb79fa8189c02
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-27 20:07:13 +0300

    added the solution for the step 89 from freeCodeCamp

---
+++ task84.js
@@ -0,0 +1,13 @@
+// 1.Step 89
+// When done has reached the value of count, we want the loop to stop executing.
+
+// Inside your if body, assign the boolean false to your continueLoop variable.
+
+// The solution:
+if (done === count) {
+    continueLoop = false;
+}
+// running tests
+// tests completed
+
+// 75% complete
