5c5aa28ef94904f0cc23912e3106de98fc1796ca
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-27 20:07:46 +0300

    added the solution for the step 90 from freeCodeCamp

---
+++ task85.js
@@ -0,0 +1,9 @@
+// 1.Step 90
+// To make your pyramid generate again, push the result of calling padRow with done and count as the arguments to your rows array, similar to what you did in your first loop.
+
+// The solution:
+rows.push(padRow(done, count));
+// running tests
+// tests completed
+
+// 76% complete
