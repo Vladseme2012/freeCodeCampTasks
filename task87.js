e7d9c4ddf9dae39f9edb9d611f11efa8ebdecb98
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-28 10:23:10 +0300

    task87.js

---
+++ task87.js
@@ -0,0 +1,22 @@
+// 1.Step 92
+// Since you have moved the comparison into the while condition, you can remove your entire if statement.
+
+// The solution:
+while (done !== count) {
+    done++;
+    rows.push(padRow(done, count));
+}
+// running tests
+// tests completed
+// console output
+
+// #
+// ###
+// #####
+// #######
+// #########
+// ###########
+// #############
+// ###############
+
+// 77% complete
