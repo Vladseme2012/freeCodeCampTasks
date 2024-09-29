760380f82ead07ea50472cd843eb494c1d20ddb1
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-29 11:33:56 +0300

    added the solutijn for the step 99 from freeCodeCamp

---
+++ task94.js
@@ -0,0 +1,16 @@
+// 1.Step 99
+// What if you made your pyramid upside-down, or inverted? Time to try it out!
+
+// Start by creating a new for loop. Declare your iterator i and assign it the value of count, then use the boolean false for your condition and iteration statements.
+
+// The solution:
+
+/*while (rows.length < count) {
+  rows.push(padRow(rows.length + 1, count));
+}*/
+
+// running tests
+// tests completed
+// console output
+
+// 83% complete
