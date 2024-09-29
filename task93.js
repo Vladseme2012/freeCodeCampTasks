ac6243af47265893b94e96d1c0b9035b438828b3
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-29 11:33:28 +0300

    added the solutijn for the step 98 from freeCodeCamp

---
+++ task93.js
@@ -0,0 +1,14 @@
+// 1.Step 99
+// What if you made your pyramid upside-down, or inverted? Time to try it out!
+
+// Start by creating a new for loop. Declare your iterator i and assign it the value of count, then use the boolean false for your condition and iteration statements.
+
+// The solution:
+
+for (let i = count; false; false) {}
+
+// running tests
+// tests completed
+// console output
+
+// 84% complete
