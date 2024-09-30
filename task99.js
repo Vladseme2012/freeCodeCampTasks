48acfba04655ae6a2d371ead7b417975e30b2afd
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-30 18:40:49 +0300

    added the solutijn for the step 104 from freeCodeCamp

---
+++ task99.js
@@ -0,0 +1,16 @@
+// 1.Step 104
+// Because you are only subtracting one from i, you can use the decrement operator --.
+
+// Replace your subtraction assignment with the decrement operator.
+
+// The solution:
+
+for (let i = count; i > 0; i--) {
+    rows.push(padRow(i, count));
+}
+
+// running tests
+// tests completed
+// console output
+
+// 88% complete
