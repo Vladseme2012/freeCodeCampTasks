fe365d4ab19601e02e59425541fe606847cb4d4c
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-30 18:35:01 +0300

    added the solutijn for the step 101 from freeCodeCamp

---
+++ task96.js
@@ -0,0 +1,13 @@
+// 1.Step 101
+// Your iteration statement is also going to be different. Instead of adding 1 to i with each loop, you need to subtract 1.
+
+// Like you did earlier with i = i + 1, update your iteration statement to give i the value of subtracting 1 from itself.
+
+// The solution:
+
+for (let i = count; i > 0; i = i - 1) {}
+
+// running tests
+// tests completed
+
+// 86% complete
