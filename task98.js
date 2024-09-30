1d8461d95fa20c4825f8f22e5465c89f23705127
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-30 18:39:24 +0300

    added the solutijn for the step 103 from freeCodeCamp

---
+++ task98.js
@@ -0,0 +1,16 @@
+// 1.Step 103
+// Just like addition, there are different operators you can use for subtraction. The subtraction assignment operator -= subtracts the given value from the current variable value, then assigns the result back to the variable.
+
+// Replace your iterator statement with the correct statement using the subtraction assignment operator.
+
+// The solution:
+
+for (let i = count; i > 0; i -= 1) {
+  rows.push(padRow(i, count));
+}
+
+// running tests
+// tests completed
+// console output
+
+// 86% complete
