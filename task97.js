5d71aeabaa3fbfe7203511e276fa726e65488f01
vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-30 18:37:01 +0300

    added the solutijn for the step 102 from freeCodeCamp

---
+++ task97.js
@@ -0,0 +1,14 @@
+// 1.Step 102
+// Again, push the result of calling padRow with your i and count variables to your rows array.
+
+// Open up the console to see the upside-down pyramid.
+
+// The solution:
+
+for (let i = count; i > 0; i = i - 1) {}
+
+// running tests
+// tests completed
+// console output
+
+// 86% complete
