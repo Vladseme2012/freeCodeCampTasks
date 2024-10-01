68ba0dc97007a702ded80e9334ef4deadc4602af
vladseme2012 <vlad.seme2012@yandex.ru>
2024-10-01 18:32:27 +0300

    added the solutijn for the step 107 from freeCodeCamp

---
+++ task101.js
@@ -0,0 +1,24 @@
+// 1.Step 107
+// The .unshift() method of an array allows you to add a value to the beginning of the array, unlike .push() which adds the value at the end of the array. .unshift() returns the new length of the array it was called on.
+
+// Example Code
+// const countDown = [2, 1, 0];
+// const newLength = countDown.unshift(3);
+// console.log(countDown); // [3, 2, 1, 0]
+// console.log(newLength); // 4
+// Use const to declare an unshifted variable, and assign it the result of calling .unshift() on your numbers array. Pass 5 as the argument. Then print your unshifted variable.
+
+// The solution:
+
+const numbers = [1, 2, 3];
+console.log(numbers);
+const unshifted = numbers.unshift(5);
+console.log(unshifted);
+
+// running tests
+// tests completed
+// console output
+[1, 2, 3];
+4;
+
+// 91% complete
