212b2e4e301ec084f1dcccdfb8815ef0e29d80de
vladseme2012 <vlad.seme2012@yandex.ru>
2024-10-01 18:35:10 +0300

    added the solutijn for the step 108 from freeCodeCamp

---
+++ task103.js
@@ -0,0 +1,14 @@
+// 1.Step 108
+// Now that you've tried these methods, you can do another inverted pyramid approach. But first you need to clean up your experimentation.
+
+// Remove your numbers array, and the method calls and log calls.
+
+// The solution:
+
+
+
+// running tests
+// tests completed
+// console output
+
+// 92% complete
