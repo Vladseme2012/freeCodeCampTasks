1e4573dce45c5abf0b039d71d37743cdd8549c0e
vladseme2012 <vlad.seme2012@yandex.ru>
2024-10-01 18:33:40 +0300

    added the solutijn for the step 107 from freeCodeCamp

---
+++ task102.js
@@ -0,0 +1,26 @@
+// 1.Step 108
+// Arrays also have a .shift() method. This will remove the first element of the array, unlike .pop() which removes the last element. Here is an example of the .shift() method:
+
+// Example Code
+// const numbers = [1, 2, 3];
+// numbers.shift();
+// The numbers array would be [2, 3].
+
+// Directly below your numbers array, declare a shifted variable and assign it the result of calling .shift() on the numbers array. On the next line, log the shifted variable to the console.
+
+// The solution:
+
+const numbers = [1, 2, 3];
+const shifted = numbers.shift();
+console.log(shifted);
+const unshifted = numbers.unshift(5);
+console.log(unshifted);
+console.log(numbers);
+
+// running tests
+// tests completed
+// console output
+1;
+(3)[(5, 2, 3)];
+
+// 92% complete
