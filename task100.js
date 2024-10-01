b2ce9aaec81086e5840f25f77b89b890ee6009c9
vladseme2012 <vlad.seme2012@yandex.ru>
2024-10-01 18:31:28 +0300

    added the solutijn for the step 105 from freeCodeCamp

---
+++ task100.js
@@ -0,0 +1,16 @@
+// 1.Step 106
+// You can actually build the inverted pyramid without needing to loop "backwards" like you did.
+
+// To do this, you'll need to learn a couple of new array methods. Start by using const to declare a numbers variable. Assign it an array with the elements 1, 2, and 3. Then log the numbers array.
+
+// The solution:
+
+const numbers = [1, 2, 3];
+console.log(numbers);
+
+// running tests
+// tests completed
+// console output
+[ 1, 2, 3 ]
+
+// 90% complete
