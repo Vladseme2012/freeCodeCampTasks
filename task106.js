757d4c7bdd3409066e91de2fb6e8d3774684d29f
vladseme2012 <vlad.seme2012@yandex.ru>
2024-10-02 19:02:38 +0300

    added the solutijn for the step 111 from freeCodeCamp

---
+++ task106.js
@@ -0,0 +1,21 @@
+// 1.Step 111
+// Your pyramid is no longer inverted. This is because you are adding new rows to the end of the array.
+
+// Update your loop body to add new rows to the beginning of the array.
+
+// The solution:
+let inverted = true;
+
+// running tests
+// tests completed
+// console output
+
+// ###############
+//  ############# 
+//   ###########  
+//    #########   
+//     #######    
+//      #####     
+//       ###      
+//        #     
+// 95% complete
