98d0185cf648f491e95411fb0348b87a94e851c6
vladseme2012 <vlad.seme2012@yandex.ru>
2024-10-04 18:27:16 +0300

    added the solutijn for the step 117 from freeCodeCamp

---
+++ task112.js
@@ -0,0 +1,28 @@
+// 1.Step 117
+// Nice work! Experiment with different values for your character, count, and inverted variables.
+
+// When you are ready to move on to your next project, set character to "!", count to 10, and inverted to false to continue.
+
+// Congratulations on completing your first JavaScript project!
+
+// The solution:
+const character = "!";
+const count = 10;
+const rows = [];
+let inverted = false;
+
+// running tests
+// tests completed
+// console output
+
+// !         
+// !!!        
+// !!!!!       
+// !!!!!!!      
+// !!!!!!!!!     
+// !!!!!!!!!!!    
+// !!!!!!!!!!!!!   
+// !!!!!!!!!!!!!!!  
+// !!!!!!!!!!!!!!!!! 
+// !!!!!!!!!!!!!!!!!!!
+// 100% complete
